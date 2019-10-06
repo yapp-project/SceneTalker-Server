84f82b91cf3d91991b4c0c9611b28d30bf172b38
SimJunSik <wnstlr24@naver.com>
2019-10-06 16:50:00 +0900

    add drf setting & user drf

--- SceneTalker/settings.py
+++ SceneTalker/settings.py
@@ -37,6 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'rest_auth.registration',
+    'rest_auth',
+    'rest_framework',
+    'rest_framework.authtoken',
+    
     'user',
 ]
 
@@ -120,3 +129,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# DRF
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ]
+}
+
+ACCOUNT_EMAIL_REQUIRED = False
--- user/views.py
+++ user/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+# from django.contrib.auth.models import User, Group
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+from user.serializers import UserSerializer
+
+User = get_user_model()
+
+class UserViewSet(APIView) :
+
+    def get_user(self, username) :
+
+        try :
+            found_user = User.objects.get(username=username)
+            return found_user
+
+        except User.DoesNotExist :
+            return None
+
+    def get(self, request, username, format=None) :
+
+        found_user = self.get_user(username)
+
+        if found_user is None :
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = UserSerializer(found_user)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
