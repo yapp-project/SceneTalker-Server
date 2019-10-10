85423e5a0a2ac7f8510781d1fb7bda41c344027f
chohongjae <bjq913@naver.com>
2019-10-10 15:56:27 +0900

    드라마 앱 url 및 설치된 앱에 추가

--- SceneTalker/settings.py
+++ SceneTalker/settings.py
@@ -51,6 +51,8 @@ INSTALLED_APPS = [
 
     'user',
     'chat',
+    'drama',
+    'taggit',
 ]
 
 MIDDLEWARE = [
--- SceneTalker/urls.py
+++ SceneTalker/urls.py
@@ -3,14 +3,12 @@ from rest_framework import routers
 from django.contrib import admin
 from user import views
 
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path("user/", include("user.urls")),
-    path("chat/", include("chat.urls")),
+    path('user/', include('user.urls')),
+    path('chat/', include('chat.urls')),
+    path('drama/', include('drama.urls'))
 ]
