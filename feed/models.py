690eebf761967db66e8be0683f5ba851be43c26b
chohongjae <bjq913@naver.com>
2019-10-23 00:18:32 +0900

    like 모델 추가

--- feed/models.py
+++ feed/models.py
@@ -12,14 +12,15 @@ class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     content = models.TextField()
     image = models.ImageField()  # s3에 올라가게 해야함
-    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='likes')
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, blank=True, related_name='likes')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         ordering = ["-created_at"]
 
-    def get_likes_count(self):
+    @property
+    def like_count(self):
         return self.likes.count()
 
 
@@ -29,3 +30,18 @@ class Comment(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Like(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        unique_together = (
+            ('user', 'post')
+        )
