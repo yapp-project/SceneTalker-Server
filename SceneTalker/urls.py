66b312208b31f1f7d2770d4dcd47f4bd704094e2
chohongjae <bjq913@naver.com>
2019-11-02 11:51:47 +0900

    api 문서를 위한 drf-yasg url 추가

--- SceneTalker/urls.py
+++ SceneTalker/urls.py
@@ -1,7 +1,22 @@
 from django.urls import include, path
-from rest_framework import routers
+from django.conf.urls import url
 from django.contrib import admin
-from user import views
+from drf_yasg.views import get_schema_view
+from rest_framework.permissions import AllowAny
+from drf_yasg import openapi
+
+
+schema_view_v1 = get_schema_view(
+    openapi.Info(
+        title="ScenteTalker API",
+        default_version='v1',
+        description="ScenteTalker Open API 문서 페이지 입니다.",
+        contact=openapi.Contact(email="bjq913@gmail.com"),
+    ),
+    validators=['flex'],
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -12,4 +27,9 @@ urlpatterns = [
     path('chat/', include('chat.urls')),
     path('drama/', include('drama.urls')),
     path('feed/', include('feed.urls')),
+
+    # Auto DRF API docs
+    url(r'^swagger(?P<format>\.json|\.yaml)/v1$', schema_view_v1.without_ui(cache_timeout=0), name='schema-json'),
+    url(r'^swagger/v1/$', schema_view_v1.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    url(r'^redoc/v1/$', schema_view_v1.with_ui('redoc', cache_timeout=0), name='schema-redoc-v1'),
 ]
