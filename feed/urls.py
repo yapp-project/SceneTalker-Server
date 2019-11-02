682c384feaafe2b0c27bd1a05a5d1fa57c1f499d
chohongjae <bjq913@naver.com>
2019-11-02 12:06:41 +0900

    feed url 추가

--- feed/urls.py
+++ feed/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 app_name = 'feed'
 urlpatterns = [
-    path('<int:feed_id>/post/', views.PostListCreateAPIView.as_view()),
-    path('<int:feed_id>/post/<int:post_id>/', views.PostRetrieveUpdateDestroyAPIView.as_view()),
-    path('<int:feed_id>/post/<int:post_id>/comment/', views.CommentListCreateAPIView.as_view()),
-    path('<int:feed_id>/post/<int:post_id>/comment/<int:comment_id>/', views.CommentRetrieveUpdateDestroyAPIView.as_view()),
+    path('<int:feed>/post/', views.PostListCreateAPIView.as_view()),
+    path('<int:feed>/post/<int:pk>/', views.PostRetrieveUpdateDestroyAPIView.as_view()),
+    path('<int:feed>/post/<int:post>/comment/', views.CommentListCreateAPIView.as_view()),
+    path('<int:feed>/post/<int:post>/comment/<int:pk>/', views.CommentRetrieveUpdateDestroyAPIView.as_view()),
 ]
