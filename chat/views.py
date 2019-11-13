ef00b33b2295ae59f9d895374a15f2056a0ef016
SimJunSik <wnstlr24@naver.com>
2019-11-13 10:53:19 +0900

    add features/get_drama_each_epsiode_count

--- chat/views.py
+++ chat/views.py
@@ -3,6 +3,10 @@ from django.utils.safestring import mark_safe
 import json
 from django.http import JsonResponse
 from channels.layers import get_channel_layer
+from rest_framework.views import APIView
+from drama.models import *
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 def room(request, room_name, user_name):
@@ -16,3 +20,15 @@ def room(request, room_name, user_name):
                 }
 
     return render(request, 'chat/room.html', context)
+
+class GetDramaEachEpisodeCount(APIView) :
+
+    def get(self, request, drama_id, episode, format=None) :
+        try :
+            drama_each_episode = DramaEachEpisode.objects.get(drama__id=drama_id, episode=episode)
+        except :
+            return Response(status=status.HTTP_404_NOT_FOUND)
+
+        serializer = DramaEachEpisodeSerializer(drama_each_episode)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
