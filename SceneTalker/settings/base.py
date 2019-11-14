5b59a5aaa51ee689407bfc9e63300a278f1f2a6e
chohongjae <bjq913@naver.com>
2019-11-15 00:25:10 +0900

    crontab 오류체크

--- SceneTalker/settings/base.py
+++ SceneTalker/settings/base.py
@@ -159,7 +159,7 @@ CHANNEL_LAYERS = {
 }
 
 CRONJOBS = [
-    ('0 6,18 * * *', 'drama.utils.crawler.update_drama', '>> /home/ubuntu/scenetalker/SceneTalker-Server/drama/utils/update.log 2 >&1'),
+    ('26 12 * * *', 'drama.utils.crawler.update_drama', '> /home/ubuntu/scenetalker/SceneTalker-Server/drama/utils/update.log 2 >&1'),
 ]
 CRONTAB_LOCK_JOBS = True
 CRONTAB_DJANGO_SETTINGS_MODULE = 'SceneTalker.settings.production'
