c36561ed487a7c39e441e0b57a84b95908cf86f8
chohongjae <bjq913@naver.com>
2019-11-15 00:32:06 +0900

    django crontab은 ,로 여러 시간을 못받는듯하여 12 업데이트로만 설정

--- SceneTalker/settings/base.py
+++ SceneTalker/settings/base.py
@@ -159,7 +159,7 @@ CHANNEL_LAYERS = {
 }
 
 CRONJOBS = [
-    ('31 0 * * *', 'drama.utils.crawler.update_drama', '> /home/ubuntu/scenetalker/SceneTalker-Server/drama/utils/update.log 2 >&1'),
+    ('0 12 * * *', 'drama.utils.crawler.update_drama', '> /home/ubuntu/scenetalker/SceneTalker-Server/drama/utils/update.log 2 >&1'),
 ]
 CRONTAB_LOCK_JOBS = True
 CRONTAB_DJANGO_SETTINGS_MODULE = 'SceneTalker.settings.production'
