ca6d86236f4de3e4e6452b8a569c91a8ea87bd45
SimJunSik <wnstlr24@naver.com>
2020-02-20 12:59:53 +0900

    pagination size 수정

--- SceneTalker/settings/base.py
+++ SceneTalker/settings/base.py
@@ -136,7 +136,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # DRF
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 20,
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ]
